b8d0f4ff3e67867216a20202b42d7e5b8a7c37ec
Ashikur Rahman <ashikur.jebu111@gmail.com>
2025-03-15 03:33:15 +0600

    Enhance Hero component with animations and drag functionality

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,27 +1,55 @@
 "use client";
 
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Hero = () => {
+    const constraintsRef = useRef(null);
+
     return (
         <section
             className="py-24"
             style={{ cursor: 'url("/cursor-you.svg"), auto' }}
+            ref={constraintsRef}
         >
             <div className="max-w-[1200px] mx-auto text-center px-16 relative">
                 <div className="hidden lg:block">
+                    {/* Left */}
                     <div className="">
-                        <div className="absolute -left-32 top-16 hidden lg:block cursor-grab">
-                            <Image
+                        <motion.div
+                            className="absolute -left-32 top-16 hidden lg:block cursor-grab"
+                            initial={{ x: -100, y: 100, opacity: 0 }}
+                            animate={{ x: 0, y: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.2,
+                                ease: "easeInOut",
+                                duration: 0.5,
+                            }}
+                        >
+                            <motion.img
                                 src="/design-example-1.webp"
                                 width={318}
                                 height={467}
                                 alt="design-example-1"
+                                drag
+                                dragConstraints={{
+                                    top: 0,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                }}
                             />
-                        </div>
-                        <div
+                        </motion.div>
+                        <motion.div
                             className="absolute left-56 top-96"
-                            style={{ opacity: 1, transform: "none" }}
+                            initial={{ x: -200, y: 100, opacity: 0 }}
+                            animate={{ x: 0, y: 0, opacity: 1 }}
+                            transition={{
+                                delay: 0.2,
+                                ease: "easeInOut",
+                                duration: 0.5,
+                            }}
                         >
                             <div className="relative">
                                 <svg
@@ -31,9 +59,9 @@ const Hero = () => {
                                     viewBox="0 0 24 24"
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     className="feather feather-mouse-pointer text-white size-5"
                                 >
                                     <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path>
@@ -45,10 +73,10 @@ const Hero = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
 
-                    {/* 2 */}
+                    {/* right */}
                     <div className="">
                         <div className="absolute -right-64 -top-16 hidden lg:block cursor-grab">
                             <Image
@@ -67,9 +95,9 @@ const Hero = () => {
                                     viewBox="0 0 24 24"
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     className="feather feather-mouse-pointer text-white size-5"
                                 >
                                     <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path>
